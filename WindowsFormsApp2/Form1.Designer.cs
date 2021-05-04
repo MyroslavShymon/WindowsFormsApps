615d5df7292bfff3244a4163c0d11c3fab9d426c
MyroslavShymon <myroslavshymon@gmail.com>
2021-05-04 23:09:39 +0300

    WindowsFormsApp1

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::WindowsFormsApp2.Properties.Resources.Screenshot_73;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 328);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(764, 119);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(237, 9);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            this.textBox1.MouseHover += new System.EventHandler(this.textBox1_MouseHover_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(12, 37);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(379, 260);
+            this.listBox1.TabIndex = 5;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 16;
+            this.listBox2.Location = new System.Drawing.Point(397, 37);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(379, 260);
+            this.listBox2.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(357, 299);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(779, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char number = e.KeyChar;
+            if ((e.KeyChar <= 47 || e.KeyChar >= 58) && number!=8) //цифры, клавиша BackSpace и запятая а ASCII
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+            int mnozhenya = 1, k = 0, rez1 = 0, rez2 = 0;//далі створбєм чотири змінних перша для того, щоб записувати добуток
+                                                         //друга для того, щоб рахувати кількість позитивних елементів масиву
+                                                         //rez1 i rez2 для того, щоб записувати в них 2 і 4 додатні елементи
+            int size_array;
+            if (textBox1.Text == "")
+            {
+                textBox1.Text = "5";
+                size_array = System.Convert.ToInt32(textBox1.Text);
+            }
+            else
+            {
+                size_array = System.Convert.ToInt32(textBox1.Text);//конвертуєм розмір масиву в textBox1
+
+            }
+            int[] sorted_array = new int[size_array];//створюємо масив
+            int max = sorted_array[0];//створюємо змінну max, в яку записуєм перший елемент масиву
+            listBox1.Items.Add("");//Вставляємо пробіл
+            listBox2.Items.Add("");//Вставляємо пробіл
+            Random rand = new Random();//Створюємо рандом
+            for (int counter = 0; counter < size_array; counter++)//створюєо цикл для перерахунку елементів масиву
+            {
+                sorted_array[counter] = rand.Next(-100, 101);//елементу масиву призначаєм значення рандому в межах від -100 до 101
+                listBox1.Items.Add(Convert.ToString(sorted_array[counter] + "\n"));//і виводимо його в listBox1
+            }
+
+
+
+            for (int counter = 0; counter < size_array; counter++)//створюєо цикл для перерахунку елементів з довжиною масива
+            {
+                if (sorted_array[counter] < 0 && counter % 2 != 0)//якщо елемент масиву менший 0 і порядковий номер не є парним
+                {
+                    mnozhenya *= sorted_array[counter];//тоді перемножаєм елементи
+                }
+                if (sorted_array[counter] > 0)//якщо елемент масиву більший 0
+                {
+                    k++;//додаєм до змінної 1
+                    if (k == 2) rez1 = counter + 1;//коли цей порядковий номер досягне двох то записуєм значення + 1 щрб користувач розумів
+                    if (k == 4) rez2 = counter + 1;//коли цей порядковий номер досягне чотирьох то записуєм значення + 1 щрб користувач розумів
+                }
+                if (sorted_array[counter] % 2 != 0)//якщо елемент масиву не є парним
+                {
+                    if (sorted_array[counter] > max) max = sorted_array[counter];//і елемент масиву більший максимального то максимпльному призначається значення елементу масива 
+                }
+
+            }
+            listBox2.Items.Add(Convert.ToString("dobutok = " + mnozhenya + "\n Elem 2 = " + rez1 + "\n Elem 4 = " + rez2 + "\n" + " max = " + max)); //виводим результат
+
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            this.Text = "Сортування масиву";//заголовок форми
+            label1.Text = "Введіть розмірність масиву";//присваюєм текст label1'у 
+            button1.Text = "Ввід";//присваюєм текст button1'у 
+        }
+
+        private void textBox1_MouseHover_1(object sender, EventArgs e)
+        {
+            toolTip1.SetToolTip(textBox1, "Введіть розмірність масиву");
+            toolTip1.IsBalloon = true;
+        }
+    }
+}
