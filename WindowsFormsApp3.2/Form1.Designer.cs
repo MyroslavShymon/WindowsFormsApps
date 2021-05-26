81a7d5e0c59b0226dafa62357d419a39753d16ba
Myroslav Bronshtein <myroslavshymon@gmail.com>
2021-05-26 21:00:12 +0300

    WindowsFormsApp3.2

---
+++ WindowsFormsApp3.2/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace WindowsFormsApp3._2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 772);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(1565, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Виводим";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click_1);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 101);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(1900, 665);
+            this.textBox1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1583, 778);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Рядки";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(1635, 772);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(1812, 772);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 8;
+            this.textBox3.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox3_KeyPress);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(1741, 775);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 17);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Колонки";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::WindowsFormsApp3._2.Properties.Resources.Screenshot_11;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(653, 68);
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1924, 797);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ WindowsFormsApp3.2/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3._2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Button1_Click_1(object sender, EventArgs e)//викликаєм функцію при кліку на Button1
+        {
+            textBox1.Text = "";
+            textBox1.ReadOnly = true;
+            int raw, col;
+            if (textBox2.Text == "")
+            {
+                textBox2.Text = "5";
+                raw = Convert.ToInt32(textBox2.Text);//конвертуєм значення з textBox2 для того щоб знати кількість рядків
+            }
+            else
+            {
+                raw = Convert.ToInt32(textBox2.Text);//конвертуєм значення з textBox2 для того щоб знати кількість рядків
+            }
+            if (textBox3.Text == "")
+            {
+                textBox3.Text = "5";
+                col = Convert.ToInt32(textBox3.Text);//конвертуєм значення з textBox3 для того щоб знати кількість стовбців
+            }
+            else
+            {
+                col = Convert.ToInt32(textBox3.Text);//конвертуєм значення з textBox3 для того щоб знати кількість стовбців
+            }
+            if (raw >= 28) {
+                textBox2.Text = "28";
+                MessageBox.Show("Максимально допустиме значення 28 ",
+        "", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                raw = 28;
+            }
+            if (col >= 28)
+            {
+                textBox3.Text = "28";
+                MessageBox.Show("Максимально допустиме значення 28 ",
+        "", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                col = 28;
+            }
+
+            double[,] a = new double[raw, col];//створюєм двохвимірний масив з введения рядків і стовбців
+            int i, j;//створюєм допоміжні цикли для змінних
+            double dobutok = 1;//створюєм змінну для обчислення добутку
+            for (i = 0; i < raw; i++)//створюєо цикл для перерахунку елементів масиву рядків
+            {
+                for (j = 0; j < col; j++)//створюєо цикл для перерахунку елементів масиву ствобців
+                {
+                    a[i, j] = i * j;//рахуєм
+                    if (a[i, j] >= 2 && a[i, j] <= 10)//якшо елемент більший або рівний 2 і менший або рівний 10
+                    {
+                        dobutok *= a[i, j];//обчислюєм добуток
+                    }
+                    textBox1.AppendText(Convert.ToString(a[i, j]) + "\t");//ввиводим масив
+                }
+                textBox1.Text += "\r\n" + "\r\n";//ввиводим масив
+            }
+            textBox1.AppendText("Добуток едементів які більші 2 і менше 10 = " + Convert.ToString(dobutok));//виводиим в кінці добуток едементів які більші 2 і менше 10
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char number = e.KeyChar;
+            if (!Char.IsDigit(number) && number != 8) // цифры и клавиша BackSpace
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void textBox3_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char number = e.KeyChar;
+            if (!Char.IsDigit(number) && number != 8) // цифры и клавиша BackSpace
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
